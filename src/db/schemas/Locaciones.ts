4e0f5e6ca82ee0a52f1981e29468a03663af011a
karimalisahili <kasahili.20@est.ucab.edu.ve>
2025-04-15 10:22:10 -0400

    faltaba especificar explicitamente mayuscula de campo idFranquicia

--- src/db/schemas/Locaciones.ts
+++ src/db/schemas/Locaciones.ts
@@ -4,7 +4,7 @@ import { Franquicias } from "./Franquicias";
 export const Locaciones = pgTable("Locaciones", {
   id: serial().primaryKey(),
   descripcion: integer().notNull(),
-  idFranquicia: integer().references(() => Franquicias.id, {
+  idFranquicia: integer("idFranquicia").references(() => Franquicias.id, {
     onUpdate: "cascade",
     onDelete: "restrict",
   }),
