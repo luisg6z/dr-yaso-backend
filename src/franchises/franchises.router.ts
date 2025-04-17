31b37c6d2a92d571d62310d13e25b0172c4b225d
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2025-04-17 18:32:43 -0400

    feat: pagination and active franchises route

--- src/franchises/franchises.router.ts
+++ src/franchises/franchises.router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createFranchiseHandler, deleteFranchiseHandler, getAllFranchisesHandler, getFranchiseByIdHandler, updateFranchiseHandler } from "./franchises.controller";
+import { createFranchiseHandler, deleteFranchiseHandler, getActivesFranchisesHandler, getAllFranchisesHandler, getFranchiseByIdHandler, updateFranchiseHandler } from "./franchises.controller";
 import { validate } from "../middleware/validate";
 import { createFranchiseSchema, updateFranchiseSchema } from "./franchises.schemas";
 
@@ -7,7 +7,8 @@ import { createFranchiseSchema, updateFranchiseSchema } from "./franchises.schem
 const franchiseRouter = Router();
 
 franchiseRouter.post("/", validate(createFranchiseSchema) ,createFranchiseHandler);
-franchiseRouter.get("/", getAllFranchisesHandler);
+franchiseRouter.get("/all", getAllFranchisesHandler);
+franchiseRouter.get("/", getActivesFranchisesHandler);
 franchiseRouter.get("/:id", getFranchiseByIdHandler);
 franchiseRouter.patch("/:id", validate(updateFranchiseSchema), updateFranchiseHandler);
 franchiseRouter.delete("/:id", deleteFranchiseHandler);
