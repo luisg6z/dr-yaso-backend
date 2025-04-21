7cb4ecb2692d4808d367aa0491125e55e4c3a8fc
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2025-04-21 19:54:16 -0400

    feat: crud visits (not tested)

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import franchiseRouter from "./franchises/franchises.router";
 import volunteersRouter from "./volunteers/volunteer.router";
 import usersRouter from "./users/users.router";
+import visitsRouter from "./visits/visits.router";
 
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/franchises", franchiseRouter);
 router.use("/volunteers", volunteersRouter);
 router.use("/users", usersRouter);
+router.use("/visits", visitsRouter);
 
 export default router;
